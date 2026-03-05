e4112fba113c43fe3b26827529f1ac79f1eb7125
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2026-03-05 02:27:30 +0200

    Add "Crash" SFX and Encapsulate player audio

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,42 +1,50 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private MovementHandler player; // Vague naming but works well :)
+    private PlayerController player; // Vague naming but works well :)
     // primarily because I'm sure only one of each script exists on the player instance
 
     void Start()
     {
-        player = GetComponent<MovementHandler>();
+        player = GetComponent<PlayerController>();
         player.isOnFloor = true;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-
-
         if(collision.gameObject.CompareTag("Floor"))
         {
             player.isOnFloor = true;
         }
         else if(collision.gameObject.CompareTag("Finish"))
         {
-            LoadNextLevel();
+            StartCoroutine(LoadNextLevel(2.0f));
         }
         else
         {
-            ReloadCurrentScene();
+            StartCoroutine(ReloadCurrentScene(2.0f));
         }
     }
 
-    void ReloadCurrentScene()
+    IEnumerator ReloadCurrentScene(float waitTime)
     {
+        player.enabled = false;
+        player.StopAllAudio();
+        player.PlayAudio("Crash");
+        yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene(GameManager.SceneIndex);
     }
 
-    void LoadNextLevel()
+    IEnumerator LoadNextLevel(float waitTime)
     {
+        player.enabled = false;
+        player.StopAllAudio();
+        //player.PlayAudio("Finish");
+        yield return new WaitForSeconds(waitTime);
         if(++GameManager.SceneIndex == SceneManager.sceneCountInBuildSettings)
         {
             GameManager.SceneIndex = 0;
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    /* Actions */
+    private InputActionMap playerActions;
+    private InputAction steerAction;
+    private InputAction thrustAction;
+    /* Actions */
+
+    /* Components */
+    #pragma warning disable
+    private Rigidbody rigidbody;
+    #pragma warning restore
+    private Dictionary<string, AudioSource> audioSources;
+    /* Components */
+
+    private float thrustPower;
+    private float steerPower;
+
+    [HideInInspector]
+    public bool isOnFloor;
+
+    void OnEnable()
+    {
+        steerAction?.Enable();
+        thrustAction?.Enable();
+    }
+
+    void Start()
+    {   
+        AudioSource[] audioSourcesArray = GetComponents<AudioSource>();
+        audioSources = new Dictionary<string, AudioSource>();
+        audioSources.Add("Thrust", audioSourcesArray[0]);
+        audioSources.Add("Crash", audioSourcesArray[1]);
+        //audioSources.Add("Finish", audioSourcesArray[2]);
+        rigidbody = GetComponent<Rigidbody>();
+
+        playerActions = InputSystem.actions.FindActionMap("Player");
+        steerAction = playerActions.FindAction("Move");
+        thrustAction = playerActions.FindAction("Jump");
+        
+        steerAction.Enable();
+        thrustAction.Enable();
+        // playerActions.Enable(); // alternatively; although not needed
+
+        thrustPower = 100.0f * Physics.gravity.magnitude * rigidbody.mass;
+        steerPower = 100.0f;
+
+        isOnFloor = true;
+    }
+
+    void FixedUpdate()
+    {
+        ProcessThrusting(Time.fixedDeltaTime);
+        ProcessSteering(Time.fixedDeltaTime);
+    }
+
+    void ProcessSteering(float deltaTime)
+    {
+        if(isOnFloor) return;
+
+        rigidbody.freezeRotation = true; // Reminds me of mutex locks :)
+        Vector2 steerInput = steerAction.ReadValue<Vector2>();
+        Vector3 steeringAxis = Vector3.Cross(Vector3.up, steerInput).normalized;
+        transform.Rotate(steerPower * deltaTime * steeringAxis); // Crossing prevents rotating on X.
+        rigidbody.freezeRotation = false;
+    }
+
+    void ProcessThrusting(float deltaTime)
+    {
+        if (thrustAction.IsPressed())
+        {
+            rigidbody.AddRelativeForce(thrustPower * deltaTime * Vector3.up);
+            if(!audioSources["Thrust"].isPlaying) audioSources["Thrust"].Play();
+        } 
+        else 
+        {
+            audioSources["Thrust"].Stop();
+        }
+    }
+
+    void OnDisable()
+    {
+        steerAction.Disable();
+        thrustAction.Disable();
+    }
+
+    public void PlayAudio(string clipName)
+    {
+        if(!audioSources[clipName].isPlaying) audioSources[clipName].Play();
+    }
+
+    public void StopAllAudio()
+    {
+        foreach(AudioSource audioSource in audioSources.Values)
+        {
+            audioSource.Stop();
+        }
+    }
+}
